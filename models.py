a68c12df0ec604faa35ded5c0425cd1cf604e15d
Sergey Nikolaev <n.strangers@gmail.com>
2023-03-29 06:57:46 +0700

    small fixes

--- models.py
+++ models.py
@@ -1,15 +1,17 @@
 class User:
-    def __init__(self):
-        self.answer = []
+    def __init__(self, answer=None):
+        if answer is None:
+            answer=[]
+        self.answer = answer
 
     def add_answer(self, answer):
         self.answer.append(answer)
 
 
 class Person(User):
-    def __init__(self):
+    def __init__(self, rating=50):
         super().__init__()
-        self.rating = 50
+        self.rating = rating
 
     def match(self, madeup, solution):
         if madeup == solution:
