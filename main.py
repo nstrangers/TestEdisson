725f0230ad4d6e16580839e30fbe6bce31fc859b
Sergey Nikolaev <n.strangers@gmail.com>
2023-04-02 22:37:39 +0700

    code rebuilding

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Form, Request, Depends
 from fastapi.responses import HTMLResponse
-from starlette.middleware.sessions import SessionMiddleware
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
 from random import randint
-import json
 
 from models import Person, User
+from serializer import serializer, deserializer
+
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key='Sergey Nikolaev')
@@ -20,47 +21,77 @@ async def get_session(request: Request):
 
 
 @app.post('/', response_class=HTMLResponse)
-async def my_post(
+async def main_loop(
         request: Request,
         answer: int = Form(default=None),
-        control: int = Form(),
+        enter_answer: int = Form(),
         session_data: dict = Depends(get_session)
 ):
-    global user
-    global extrasenses
 
-    if not session_data:
+    error_message = ''
+
+    #получение объектов
+    if session_data:
+        user = deserializer(request, 'class_user')
+        extrasenses = deserializer(request, 'class_extrasenses')
+        status = request.session.get('status')
+    else:
         extrasenses = [Person() for i in range(NUMBER_OF_EXTRASENSE)]
         user = User()
+        status = False
 
-    if answer is None and control == 0:
+    # обработка данных из формы
+    if enter_answer == 0:
         for extrasens in extrasenses:
             extrasens.add_answer(randint(10, 99))
         status = True
-        session_data['extrasenses'] = [vars(extrasens) for extrasens in extrasenses]
-        session_data['status'] = status
 
-    if answer is not None and validator(answer) and control == 1:
-        user.add_answer(answer)
-        for extrasens in extrasenses:
-            extrasens.match(extrasens.answer[-1], answer)
-        session_data['user'] = user.answer
-        session_data['extrasenses'] = [vars(extrasens) for extrasens in extrasenses]
-        status = False
-        session_data['status'] = status
-    return templates.TemplateResponse("index.html", {'request': request, 'data': session_data})
+    if enter_answer == 1:
+        try:
+            user.validate_answer(answer)
+        except TypeError:
+            error_message = 'Введенные данные должны быть целым числом'
+        except ValueError:
+            error_message = 'Число должно быть двухзначным'
+        else:
+            user.add_answer(answer)
+            for extrasens in extrasenses:
+                extrasens.match(extrasens.answer[-1], answer)
+            status = False
+
+    data = get_data_for_templates(user, extrasenses, status)
+
+    #запись данных в сессию
+    session_data['status'] = status
+    session_data['class_extrasenses'] = serializer(extrasenses)
+    session_data['class_user'] = serializer(user)
+
+    return templates.TemplateResponse('index.html',
+        {'request': request, 'data': data, 'error_message': error_message}
+    )
 
 
 @app.get('/', response_class=HTMLResponse)
-async def my_get(request: Request, session_data: dict = Depends(get_session)):
+async def formation_start_page(request: Request, session_data: dict = Depends(get_session)):
     session_data = session_data or {}
-    if not session_data:
-        session_data['status'] = False
-    return templates.TemplateResponse("index.html", {'request': request, "data": session_data})
+    data = {}
+
+    if session_data:
+        user = deserializer(request, 'class_user')
+        extrasenses = deserializer(request, 'class_extrasenses')
+        status = session_data['status']
+        data = get_data_for_templates(user, extrasenses, status)
+    else:
+        data['status'] = False
+
+    return templates.TemplateResponse("index.html", {'request': request, "data": data})
 
 
-def validator(answer):
-    return 10 <= answer <= 99
+def get_data_for_templates(user, extrasenses, status):
+    data = {'user': user.answer,
+            'extrasenses': [vars(extrasens) for extrasens in extrasenses],
+            'status': status}
+    return data
 
 
 @app.get('/clear-session')  # сброс сессии
